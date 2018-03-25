d370a444f096e1caa77b5c6300afdcd241a84368
Jeff Schomay <jschomay@gmail.com>
2018-03-25 12:38:32 -0700

    better anagram

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,8 +1,13 @@
-(ns anagram)
+(ns anagram
+  (:require  [clojure.string :as s]))
+
+(defn equal-with
+  "Makes a compairison by first running the supplied arguments through the supplied funciton."
+  [f & args]
+  (apply = (map f args)))
 
 (defn anagrams-for [word candidates]
-  (let [lower-word (clojure.string/lower-case word)
-        check #(and
-                (not= lower-word %)
-                (= (sort lower-word) (sort (clojure.string/lower-case %))))]
-    (filter check candidates)))
+  (filter #(and
+            (not (equal-with s/lower-case word %))
+            (equal-with (comp sort s/lower-case) word %))
+          candidates))
